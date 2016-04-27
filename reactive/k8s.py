dd2e87b66095a5c0bd09d10df643952d40ff9bc5
Matt Bruzek <matthew.bruzek@canonical.com>
2016-04-27 16:44:38 -0500

    Adding pillar object so templates are closer to upstream.
    Removing the copy code as we can call tlslib to do that.
    Comments and other fixes as I was in here.

--- reactive/k8s.py
+++ reactive/k8s.py
@@ -1,7 +1,6 @@
 import os
 
 from shlex import split
-from shutil import copy2
 from subprocess import call
 from subprocess import check_call
 from subprocess import check_output
@@ -19,7 +18,8 @@ from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.templating import render
 from charmhelpers.core import unitdata
 from charmhelpers.core.host import chdir
-from contextlib import contextmanager
+
+import tlslib
 
 
 @hook('config-changed')
@@ -52,13 +52,11 @@ def config_changed():
 @when('tls.server.certificate available')
 @when_not('k8s.server.certificate available')
 def server_cert():
-    '''When the server certificate is available, get the server certificate from
-    the charm unit data and write it to the proper directory. '''
+    '''When the server certificate is available, get the server certificate
+    from the charm unit data and write it to the kubernetes directory. '''
     destination_directory = '/srv/kubernetes'
-    # Save the server certificate from unitdata to /srv/kubernetes/server.crt
-    save_certificate(destination_directory, 'server')
-    # Copy the unitname.key to /srv/kubernetes/server.key
-    copy_key(destination_directory, 'server')
+    # Save the server certificate from unitdata to the destination directory.
+    tlslib.server_cert(destination_directory)
     set_state('k8s.server.certificate available')
 
 
@@ -66,44 +64,22 @@ def server_cert():
 @when_not('k8s.client.certficate available')
 def client_cert():
     '''When the client certificate is available, get the client certificate
-    from the charm unitdata and write it to the proper directory. '''
+    from the charm unitdata and write it to the kubernetes directory. '''
     destination_directory = '/srv/kubernetes'
-    if not os.path.isdir(destination_directory):
-        os.makedirs(destination_directory)
-        os.chmod(destination_directory, 0o770)
-    # The client certificate is also available on charm unitdata.
-    client_cert_path = 'easy-rsa/easyrsa3/pki/issued/client.crt'
-    kube_cert_path = os.path.join(destination_directory, 'client.crt')
-    if os.path.isfile(client_cert_path):
-        # Copy the client.crt to /srv/kubernetes/client.crt
-        copy2(client_cert_path, kube_cert_path)
-    # The client key is only available on the leader.
-    client_key_path = 'easy-rsa/easyrsa3/pki/private/client.key'
-    kube_key_path = os.path.join(destination_directory, 'client.key')
-    if os.path.isfile(client_key_path):
-        # Copy the client.key to /srv/kubernetes/client.key
-        copy2(client_key_path, kube_key_path)
+    # Copy the client certificate and key to the destination directory.
+    tlslib.client_cert(destination_directory)
+    set_state('k8s.client.certficate available')
 
 
 @when('tls.certificate.authority available')
 @when_not('k8s.certificate.authority available')
 def ca():
     '''When the Certificate Authority is available, copy the CA from the
-    /usr/local/share/ca-certificates/k8s.crt to the proper directory. '''
-    # Ensure the /srv/kubernetes directory exists.
-    directory = '/srv/kubernetes'
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-        os.chmod(directory, 0o770)
-    # Normally the CA is just on the leader, but the tls layer installs the
-    # CA on all systems in the /usr/local/share/ca-certificates directory.
-    ca_path = '/usr/local/share/ca-certificates/{0}.crt'.format(
-              hookenv.service_name())
-    # The CA should be copied to the destination directory and named 'ca.crt'.
-    destination_ca_path = os.path.join(directory, 'ca.crt')
-    if os.path.isfile(ca_path):
-        copy2(ca_path, destination_ca_path)
-        set_state('k8s.certificate.authority available')
+    /usr/local/share/ca-certificates/k8s.crt to the kubernetes directory. '''
+    destination_directory = '/srv/kubernetes'
+    # Copy the Certificate Authority to the destination directory.
+    tlslib.ca(destination_directory)
+    set_state('k8s.certificate.authority available')
 
 
 @when('kubelet.available', 'proxy.available', 'cadvisor.available')
@@ -130,7 +106,8 @@ def launch_skydns():
     return_code = call(split('kubectl cluster-info'))
     if return_code != 0:
         hookenv.log('kubectl command failed, waiting for apiserver to start.')
-        # Return without setting the available state so this method will retry.
+        remove_state('skydns.available')
+        # Return without setting skydns.available so this method will retry.
         return
     # Check for the "kube-system" namespace.
     return_code = call(split('kubectl get namespace kube-system'))
@@ -256,44 +233,46 @@ def start_cadvisor():
 def gather_sdn_data():
     '''Get the Software Defined Network (SDN) information and return it as a
     dictionary.'''
+    sdn_data = {}
+    # The pillar dictionary is a construct of the skydns files.
+    pillar = {}
     # SDN Providers pass data via the unitdata.kv module
     db = unitdata.kv()
-    # Generate an IP address for the DNS provider
+    # Ideally the DNS address should come from the sdn cidr.
     subnet = db.get('sdn_subnet')
     if subnet:
-        ip = subnet.split('/')[0]
-        dns_server = '.'.join(ip.split('.')[0:-1]) + '.10'
-        addedcontext = {}
-        addedcontext['dns_server'] = dns_server
-        return addedcontext
-    return {}
+        # Generate the DNS ip address on the SDN cidr (this is desired).
+        pillar['dns_server'] = get_dns_ip(subnet)
+    else:
+        # There is no SDN cider fall back to the kubernetes config cidr option.
+        pillar['dns_server'] = get_dns_ip(hookenv.config().get('cidr'))
+    # The pillar['dns_server'] value is used the skydns-svc.yml file.
+    pillar['dns_replicas'] = 1
+    # The pillar['dns_domain'] value is ued in the skydns-rc.yml
+    pillar['dns_domain'] = hookenv.config().get('dns_domain')
+    # Use a 'pillar' dictionary so we can reuse the upstream skydns templates.
+    sdn_data['pillar'] = pillar
+    return sdn_data
 
 
-def copy_key(directory, prefix):
-    '''Copy the key from the easy-rsa/easyrsa3/pki/private directory to the
-    specified directory. '''
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-        os.chmod(directory, 0o770)
-    # Must remove the path characters from the local unit name.
-    path_name = hookenv.local_unit().replace('/', '_')
-    # The key is not in unitdata it is in the local easy-rsa directory.
-    local_key_path = 'easy-rsa/easyrsa3/pki/private/{0}.key'.format(path_name)
-    key_name = '{0}.key'.format(prefix)
-    # The key should be copied to this directory.
-    destination_key_path = os.path.join(directory, key_name)
-    # Copy the key file from the local directory to the destination.
-    copy2(local_key_path, destination_key_path)
+def get_dns_ip(cidr):
+    '''Get an IP address for the DNS server on the provided cidr.'''
+    # Remove the range from the cidr.
+    ip = cidr.split('/')[0]
+    # Take the last octet off the IP address and replace it with 10.
+    return '.'.join(ip.split('.')[0:-1]) + '.10'
 
 
 def render_files(reldata=None):
     '''Use jinja templating to render the docker-compose.yml and master.json
     file to contain the dynamic data for the configuration files.'''
     context = {}
-    # Load the context manager with sdn and config data.
+    # Load the context data with SDN data.
     context.update(gather_sdn_data())
+    # Add the charm configuration data to the context.
     context.update(hookenv.config())
     if reldata:
+        # Add the etcd relation data to the context.
         context.update({'connection_string': reldata.connection_string()})
     charm_dir = hookenv.charm_dir()
     rendered_kube_dir = os.path.join(charm_dir, 'files/kubernetes')
@@ -306,21 +285,27 @@ def render_files(reldata=None):
     # Update the context with extra values, arch, manifest dir, and private IP.
     context.update({'arch': arch(),
                     'manifest_directory': rendered_manifest_dir,
+                    'public_address': hookenv.unit_get('public-address'),
                     'private_address': hookenv.unit_get('private-address')})
 
+    # Adapted from: http://kubernetes.io/docs/getting-started-guides/docker/
+    target = os.path.join(rendered_kube_dir, 'docker-compose.yml')
     # Render the files/kubernetes/docker-compose.yml file that contains the
     # definition for kubelet and proxy.
-    target = os.path.join(rendered_kube_dir, 'docker-compose.yml')
     render('docker-compose.yml', target, context)
+
+    # Source: https://github.com/kubernetes/...master/cluster/images/hyperkube
+    target = os.path.join(rendered_manifest_dir, 'master.json')
     # Render the files/manifests/master.json that contains parameters for the
     # apiserver, controller, and controller-manager
-    target = os.path.join(rendered_manifest_dir, 'master.json')
     render('master.json', target, context)
-    # Render files/kubernetes/skydns-svc.yaml for SkyDNS service
+    # Source: ...master/cluster/addons/dns/skydns-svc.yaml.in
     target = os.path.join(rendered_manifest_dir, 'skydns-svc.yml')
+    # Render files/kubernetes/skydns-svc.yaml for SkyDNS service.
     render('skydns-svc.yml', target, context)
-    # Render files/kubernetes/skydns-rc.yaml for SkyDNS pods
+    # Source: ...master/cluster/addons/dns/skydns-rc.yaml.in
     target = os.path.join(rendered_manifest_dir, 'skydns-rc.yml')
+    # Render files/kubernetes/skydns-rc.yaml for SkyDNS pod.
     render('skydns-rc.yml', target, context)
 
 
@@ -337,21 +322,3 @@ def arch():
         status_set('blocked', message)
         raise Exception(message)
     return architecture
-
-
-def save_certificate(directory, prefix):
-    '''Get the certificate from the charm unitdata, and write it to the proper
-    directory. The parameters are: destination directory, and prefix to use
-    for the key and certificate name.'''
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-        os.chmod(directory, 0o770)
-    # Grab the unitdata key value store.
-    store = unitdata.kv()
-    certificate_data = store.get('tls.{0}.certificate'.format(prefix))
-    certificate_name = '{0}.crt'.format(prefix)
-    # The certificate should be saved to this directory.
-    certificate_path = os.path.join(directory, certificate_name)
-    # write the server certificate out to the correct location
-    with open(certificate_path, 'w') as fp:
-        fp.write(certificate_data)
